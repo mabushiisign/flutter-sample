68c91d368a231336155c1e1bd18ce4a932cb7538
Takayoshi Nozaki <mabushii.sign@gmail.com>
2021-04-10 16:16:05 +0900

    first commit

---
+++ flutter_spring/src/test/java/com/mabushiisign/flutter_spring/FlutterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabushiisign.flutter_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
