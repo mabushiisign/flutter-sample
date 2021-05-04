78db672042431991e8088aa04565f798ddda3ef8
Takayoshi Nozaki <mabushii.sign@gmail.com>
2021-05-04 17:43:23 +0900

    Swagger UIにCookie認証の設定を追加

--- flutter_api/src/main/java/com/mabushiisign/flutter_sample/api/Application.java
+++ flutter_api/src/main/java/com/mabushiisign/flutter_sample/api/Application.java
@@ -2,9 +2,19 @@ package com.mabushiisign.flutter_sample.api;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition(info = @Info(title = "${api.title}", version = "${api.version}", description = "${api.description}"))
+@SecurityScheme(name = "CookieAuth",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.COOKIE,
+        paramName = "SESSION")
+@OpenAPIDefinition(info = @Info(
+        title = "${api.title}",
+        version = "${api.version}",
+        description = "${api.description}"))
 public class Application {
 
     public static void main(String[] args) {
